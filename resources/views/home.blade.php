3e393654da897d3ff24ac0628af7eef09ba34e67
Bryan Amds <bryan.amadeus1003@gmail.com>
2023-10-24 10:31:32 +0700

    First Commit
    ae

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,7 +4,8 @@
     @guest
         <h1>hey, please login first before using our app!</h1>
     @else
-        <h1>Hemlo {{ $user->name }}, This is ur Boards</h1>
+        <h1>Henlo {{ $user->name }}, This is ur Boards</h1>
+        <h1>Bryan was here</h1>
         <h1>----------------------------</h1>
         <h3><a href="/boards/{{ $user->id }}"> View Your Boards </a></h3>
     @endguest
