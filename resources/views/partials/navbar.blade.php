ea0252235ec483fa103fe97e43e182f61481fc4b
Gilang Kurniawan <90315796+Gilangk1902@users.noreply.github.com>
2023-12-11 19:56:36 +0700

    what am i doing bro

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -3,22 +3,19 @@
         <a class="navbar-brand text-light" href="/">Assigment Manager</a>
         
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            @auth
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                 <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle text-light" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                         Boards
                     </a>
+                    
                     <ul class="dropdown-menu">
-                        @auth
-                            @forelse (Auth::user()->boards as $board)
-                                <li><a href="/board/{{ $board->slug }}" class="dropdown-item text-decoration-none">{{ $board->title }}</a></li>
-                            @empty
-                                <li><a class="dropdown-item text-decoration-none" href="#">No boards available</a></li>
-                            @endforelse
-                        @else
-                            <li><a class="dropdown-item text-decoration-none" href="#">Please log in</a></li>
-                        @endauth
-
+                        @forelse (Auth::user()->boards as $board)
+                            <li><a href="/board/{{ $board->slug }}" class="dropdown-item text-decoration-none">{{ $board->title }}</a></li>
+                        @empty
+                            <li><a class="dropdown-item text-decoration-none" href="#">No boards available</a></li>
+                        @endforelse
                         <li><hr class="dropdown-divider"></li>
                         <li><a class="dropdown-item" href="/make_new_board">Create new Board</a></li>
                     </ul>
@@ -34,8 +31,9 @@
                     </ul>
                 </li>
             </ul>
-
-            <div class="d-flex" >
+            @endauth
+        
+            <div class="d-flex ms-auto"> <!-- Use ms-auto to push the content to the right -->
                 @guest
                     <a href="/login" class="btn btn-primary me-2">Login</a>
                     <a href="/register" class="btn btn-primary me-2">Register</a>
@@ -46,7 +44,6 @@
                 </form>
                 @endguest
             </div>
-            
         </div>
     </div>
 </nav>
